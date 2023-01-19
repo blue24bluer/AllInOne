35f9df341539ead7e734145f6b92ef664359e3b4
Blue24Bluer <119597827+blue24bluer@users.noreply.github.com>
2023-01-19 11:10:33 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-echo -e " this is your first time in this tool "
-echo -e " so you have to install some files "
-echo -e " don't worry .. we will install it ^_* "
-apt install pv -y 
-apt install toilet -y
-apt install w3m -y 
-
